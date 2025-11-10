10de94bfd19f5be663a411e58db715ff6e924f89
Koil <wcwoxingle@outlook.com>
2025-11-10 21:26:39 +0800

    Update Lab5_T2_Skeleton.ino

--- Lab5_T2_Skeleton/Lab5_T2_Skeleton.ino
+++ Lab5_T2_Skeleton/Lab5_T2_Skeleton.ino
@@ -1,14 +1,14 @@
 /*Change all the ? in the code and add code in ??? to Control 2 DC Motors.*/
 
-#define IN1   ?  // Replace the ? with the GPIO pin you selected to connect IN1
-#define IN2   ?   // Replace the ? with the GPIO pin you selected to connect IN2
-#define AL  ? // Replace the ? with the GPIO pin you selected to connect encoder A of Motor L
-#define BL  ? // Replace the ? with the GPIO pin you selected to connect encoder B of Motor L
+#define IN1   27  // Replace the ? with the GPIO pin you selected to connect IN1
+#define IN2   32   // Replace the ? with the GPIO pin you selected to connect IN2
+#define AL  34 // Replace the ? with the GPIO pin you selected to connect encoder A of Motor L
+#define BL  35 // Replace the ? with the GPIO pin you selected to connect encoder B of Motor L
 
-#define IN3   ?  // Replace the ? with the GPIO pin you selected to connect IN3
-#define IN4   ?   // Replace the ? with the GPIO pin you selected to connect IN4
-#define AR  ? // Replace the ? with the GPIO pin you selected to connect encoder A of Motor R
-#define BR ? // Replace the ? with the GPIO pin you selected to connect encoder B of Motor R
+#define IN3   14  // Replace the ? with the GPIO pin you selected to connect IN3
+#define IN4    12  // Replace the ? with the GPIO pin you selected to connect IN4
+#define AR  17 // Replace the ? with the GPIO pin you selected to connect encoder A of Motor R
+#define BR 13 // Replace the ? with the GPIO pin you selected to connect encoder B of Motor R
 
 
 int AL_data=0;
@@ -21,7 +21,16 @@ String command;
 
 void setup() {
   /*setup baud and pin mode */
-   ??? 
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(AL,INPUT);
+  pinMode(BL,INPUT);
+  pinMode(AR,INPUT);
+  pinMode(BR,INPUT);
+  Serial.begin(115200);
+  /*Set a rotation direction*/
+  // digitalWrite(PWM1,HIGH);
+  // digitalWrite(PWM2,HIGH);
 
 
 }
@@ -34,28 +43,57 @@ void loop() {
     if (command == "F") 
     { 
       // Move forward 
-       ??? 
+        digitalWrite(IN1,HIGH);
+        digitalWrite(IN2,LOW);
+        digitalWrite(IN3,HIGH);
+        digitalWrite(IN4,LOW);
       } 
     else if (command == "B") 
     { // Move Backward 
-      ??? 
+        digitalWrite(IN2,HIGH);
+        digitalWrite(IN1,LOW);
+        digitalWrite(IN4,HIGH);
+        digitalWrite(IN3,LOW);
       } 
     else if (command == "R") 
     { // Turn right 
-   ???
+
+        digitalWrite(IN1,HIGH);
+        digitalWrite(IN2,LOW);
+        digitalWrite(IN3,LOW);
+        digitalWrite(IN4,HIGH);
     } 
     else if (command == "L") { 
     // Turn left 
-    ???
+
+
+        digitalWrite(IN2,HIGH);
+        digitalWrite(IN1,LOW);
+        digitalWrite(IN4,LOW);
+        digitalWrite(IN3,HIGH);
    } 
   else if (command == "S") { 
     // Stop 
-    ???
+        digitalWrite(IN1,LOW);
+        digitalWrite(IN2,LOW);
+        digitalWrite(IN3,LOW);
+        digitalWrite(IN4,LOW);
      } 
         }
+    AL_data=digitalRead(AL);
+    BL_data=digitalRead(BL);
+    AR_data=digitalRead(AR);
+    BR_data=digitalRead(BR);
     // Read values of A and B of both Motor L and R
-     ???
+     Serial.print("AL:");
+     Serial.println(AL_data);
+     Serial.print("BL:");
+     Serial.println(BL_data);
+     Serial.print("AR:");
+     Serial.println(AR_data);
+     Serial.print("BR:");
+     Serial.println(BR_data);
     // Plot A and B of both Motor L and R in Serial Plotter
-    ???
+  
     
 }
